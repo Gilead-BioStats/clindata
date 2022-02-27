d5c833ed158ba05668dfcd1478158f9eb8c8a43a
George Wu <george.wu@gilead.coom>
2022-02-27 11:58:07 -0800

    R code update

--- data-raw/raw/prepareLabsSubset.R
+++ data-raw/raw/prepareLabsSubset.R
@@ -3,4 +3,15 @@ library(here)
 
 here::i_am("data-raw/raw/prepareLabsSubset.R.R")
 
-load("")
+#library(haven)
+#covlab <- read_sas("covlab.sas7bdat")
+
+
+sort(table(covlab$BATTRNAM))
+
+
+raw_covlab_hema <- covlab[covlab$BATTRNAM == "HEMATOLOGY&DIFFERENTIAL PANEL", ]
+table(raw_covlab_hema$LBTEST)
+table(raw_covlab_hema$TOXGRG)
+
+usethis::use_data(raw_covlab_hema, overwrite=TRUE)
