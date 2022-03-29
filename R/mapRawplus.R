5c76a000f192b737da62aa3c93eb41e4148845bf
Jeremy Wildfire <jwildfire@gmail.com>
2022-03-29 10:15:20 -0400

    add simple mappings for rawplus

---
+++ R/mapRawplus.R
@@ -0,0 +1,93 @@
+#' map_rawplus_ae 
+#' 
+#' mapping to create rawplus_ae from raw
+#' 
+#' @param  dfRawAE data frame of raw AE data
+#' 
+#' @examples 
+#' 
+#' rawplus_ae <- map_rawplus_ae(clindata::raw_ae)
+
+map_rawplus_ae <- function(dfRawAE){
+    rawplus_ae <- dfRawAE %>%
+    select(
+        SubjectID = SUBJID,
+        AE_Serious = AESER
+    ) %>%
+    mutate(AE_TE_FLAG=sample(c(TRUE,FALSE),n(),replace=TRUE)) %>% # Random TE for now - TODO update
+    mutate(AE_GRADE = sample(1:4,n(),replace=TRUE)) # Random Grade for now - TODO update
+
+    return(rawplus_ae)
+}
+
+#' map_rawplus_pd
+#' 
+#' mapping to create rawplus_pd from raw
+#' 
+#' @param  dfRawPD data frame of raw AE data
+#' 
+#' @examples
+#' 
+#' rawplus_pd <- map_rawplus_pd(clindata::raw_protdev)
+
+map_rawplus_pd <- function(dfRawAE){
+    rawplus_pd <- dfRawAE %>%
+    mutate(PD_Important_Flag = case_when(
+        DEVIMP == 'y' ~ "Y", 
+        DEVIMP == 'n' ~ "N",
+        TRUE ~ DEVIMP 
+    )) %>%
+    select(
+        SubjectID = SUBJID,
+        PD_Category = DEVTYPE,
+        PD_Important_Flag,
+    ) 
+
+    return(rawplus_pd)
+}
+
+#' map_rawplus_ie
+#' 
+#' mapping to create rawplus_ie from raw
+#' 
+#' @param  dfRawIE data frame of raw AE data
+#' 
+#' @examples 
+#' 
+#' rawplus_ie <- map_rawplus_ie(clindata::raw_ie_all)
+
+map_rawplus_ie <- function(dfRawIE){
+    rawplus_ie <- dfRawIE %>%
+    select(
+        SubjectID = SUBJID,
+        IE_Category = IECAT_STD,
+        IE_Value = IEORRES,
+        IE_ProtocolVersion = PROTVER_STD
+    ) 
+
+    return(rawplus_ie)
+}
+
+#' map_rawplus_consent
+#' 
+#' mapping to create map_rawplus_consent from raw
+#' 
+#' @param  dfRawElig data frame of raw AE data
+#' 
+#' @examples 
+#' 
+#' rawplus_consent <- map_rawplus_consent(clindata::raw_ic_elig)
+
+map_rawplus_consent <- function(dfRawElig){
+    rawplus_consent <- dfRawElig %>%
+    select(
+        SubjectID=SUBJID, 
+        Consent_Date = DSSTDAT_RAW) %>%
+    mutate(
+        Consent_Type = "MAINCONSENT",
+        Consent_Value = "Y",
+        Consent_Date = as.Date(Consent_Date, format = "%d %B %Y")
+    ) %>%
+    filter(SubjectID != "")
+    return(rawplus_consent)
+}
--- data-raw/rawplus/makeRawPlus.R
+++ data-raw/rawplus/makeRawPlus.R
@@ -1,14 +1,30 @@
 here::i_am("data-raw/rawplus/makeRawPlus.R")
 library(here)
 
-rawplus_rdsl_s <- CreateRDSL(dfDm = clindata::raw_dm,
-                           dfIXRSrand = clindata::raw_iwrsrand,
-                           dfEx = clindata::raw_ex,
-                           dfVisit = clindata::raw_visdt,
-                           dfStud = clindata::raw_studcomp,
-                           dfSdrg = clindata::raw_sdrgcomp)
+# Make RDSL
+rawplus_rdsl_s <- CreateRDSL(
+    dfDm = clindata::raw_dm,
+    dfIXRSrand = clindata::raw_iwrsrand,
+    dfEx = clindata::raw_ex,
+    dfVisit = clindata::raw_visdt,
+    dfStud = clindata::raw_studcomp,
+    dfSdrg = clindata::raw_sdrgcomp
+)
 
 rawplus_rdsl <- rawplus_rdsl %>% filter( RandFlag=="Y")
 
 usethis::use_data(rawplus_rdsl_s, overwrite=TRUE)
 usethis::use_data(rawplus_rdsl, overwrite=TRUE)
+
+
+# Domain Mappings
+
+rawplus_ae <- map_rawplus_ae(clindata::raw_ae)
+rawplus_pd <- map_rawplus_pd(clindata::raw_protdev)
+rawplus_ie <- map_rawplus_ie(clindata::raw_ie_all)
+rawplus_consent <- map_rawplus_consent(clindata::raw_ic_elig)
+
+usethis::use_data(rawplus_ae, overwrite=TRUE)
+usethis::use_data(rawplus_pd, overwrite=TRUE)
+usethis::use_data(rawplus_ie, overwrite=TRUE)
+usethis::use_data(rawplus_consent, overwrite=TRUE)
