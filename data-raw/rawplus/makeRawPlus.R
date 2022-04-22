9eb1fefbb0f4fd7335080b3b6639a351bb526e22
Matt Roumaya <40671730+mattroumaya@users.noreply.github.com>
2022-04-22 10:29:46 -0400

    Update data-raw/rawplus/makeRawPlus.R
    Co-authored-by: jwildfire <jwildfire@gmail.com>

--- data-raw/rawplus/makeRawPlus.R
+++ data-raw/rawplus/makeRawPlus.R
@@ -25,7 +25,7 @@ rawplus_ae <- map_rawplus_ae(clindata::raw_ae, ids)
 rawplus_pd <- map_rawplus_pd(clindata::raw_protdev, ids)
 rawplus_ie <- map_rawplus_ie(clindata::raw_ie_all, ids)
 rawplus_consent <- map_rawplus_consent(clindata::raw_ic_elig, ids)
-rawplus_mapping <- yaml::read_yaml(here::here("inst", "mapping", "rawplus.yaml"))
+mapping_rawplus <- yaml::read_yaml(here::here("inst", "mapping", "rawplus.yaml"))
 
 usethis::use_data(rawplus_ae, overwrite=TRUE)
 usethis::use_data(rawplus_pd, overwrite=TRUE)
