7851867141f904cd99ec84cc39ca00dff9ba9324
Spencer Childress <spencer.c.childress@gmail.com>
2022-08-18 10:12:51 -0400

    remove data cleaning of protocol deviations and map existing labs data to current rawplus standard

--- data-raw/save_parquet.R
+++ data-raw/save_parquet.R
@@ -4,8 +4,28 @@ library(stringr)
 library(arrow)
 library(usethis)
 
-dm <- system.file('data-raw', 'rawplus', 'dm.parquet', package = 'clindata') %>%
-  arrow::read_parquet()
+#dm <- system.file('data-raw', 'rawplus', 'dm.parquet', package = 'clindata') %>%
+#  arrow::read_parquet()
+
+# TODO: replace with actual rawplus export
+lb <- clindata::rawplus_lb %>%
+  select(
+    siteid = SiteID,
+    subjid = SubjectID,
+    VISNAM = VISIT,
+    VISNUM = VISITNUM,
+    LB_DT = LBDTN,
+    BATTRNAM = LBCAT,
+    LBTSTNAM = LBTEST,
+    LBTSTCD = LBTESTCD,
+    SIRESN = LBSTRESN,
+    SINRLO = LBSTNRLO,
+    SINRHI = LBSTNRHI,
+    LB_TE = LB_TE_FLAG,
+    TOXGR = LB_GRADE
+  )
+
+usethis::use_data(lb)
 
 system.file('data-raw', 'rawplus', package = 'clindata') %>% # path to ./data-raw
   list.files(
@@ -15,9 +35,15 @@ system.file('data-raw', 'rawplus', package = 'clindata') %>% # path to ./data-ra
   purrr::walk(function(file) {
     domain <- stringr::word(file, -2, sep = '/|\\.') # name of data domain
     data <- arrow::read_parquet(file) # ingest .parquet file
-    if (domain == 'protdev')
-        data = clean_protdev(data, dm)
+
+    # TODO: avoid cleaning protocol deviations
+    #
+    # Merge dm.subjectid onto protdev
+    #if (domain == 'protdev')
+    #    data = clean_protdev(data, dm)
+
     assign(domain, data) # define local variable
+
     do.call(
       'use_data',
       list(
