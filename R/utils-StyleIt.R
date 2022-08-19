edacc9572c6d056880483d45401f44d94922df36
Spencer Childress <spencer.c.childress@gmail.com>
2022-08-19 16:03:20 -0400

    document latest rawplus data

--- R/utils-StyleIt.R
+++
@@ -1,9 +0,0 @@
-StyleIt <- function() {
-  myStyle <- styler::tidyverse_style()
-
-  myStyle$indention$unindent_fun_dec <- NULL
-  myStyle$indention$update_indention_ref_fun_dec <- NULL
-  myStyle$line_break$remove_line_breaks_in_fun_dec <- NULL
-
-  styler::style_pkg(transformers = myStyle)
-}
