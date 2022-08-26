e595ffca20afbdaa9cca8f32e13ac8c91d274afd
Zhongkai-Wang <Zhongkai.Wang6@gilead.com>
2022-08-26 14:35:00 -0400

    hard code lab data with additional variables

--- data-raw/save_rawplus.R
+++ data-raw/save_rawplus.R
@@ -3,6 +3,7 @@ library(purrr)
 library(stringr)
 library(arrow)
 library(usethis)
+library(dplyr)
 
 system.file('data-raw', 'rawplus', package = 'clindata') %>% # path to ./data-raw
   list.files(
@@ -16,10 +17,26 @@ system.file('data-raw', 'rawplus', package = 'clindata') %>% # path to ./data-ra
     )
     print(domain)
 
-    data <- arrow::read_parquet(file) # ingest .parquet file
-    print(dim(data))
-
-    assign(domain, data) # define local variable
+    if (domain == "rawplus_lb"){  # hard code lb differently using lb, dm rawplus data
+      rawplus_lb <- arrow::read_parquet(file)
+      rawplus_dm <- arrow::read_parquet(list.files(system.file('data-raw', 'rawplus', package = 'clindata'),
+                                                'dm.parquet$', # retrieve .parquet files in ./data-raw
+                                                full.names = TRUE))
+      data_te <- rawplus_dm  %>%
+        select(subjid, siteid, rfxst_dt, rfxen_dt)  %>%
+        filter(subjid!="")
+      data_lb <- rawplus_lb %>%
+        left_join(data_te, by = "subjid") %>%
+        mutate(
+          lb_te = ifelse(lb_dt>=as.Date(rfxst_dt)&lb_dt<=as.Date(rfxen_dt)+30, "YES", "")
+        )
+      print(dim(data_lb))
+      assign(domain, data_lb)
+    } else {
+      data <- arrow::read_parquet(file) # ingest .parquet file
+      print(dim(data))
+      assign(domain, data) # define local variable
+    }
 
     do.call(
       'use_data',
@@ -29,3 +46,4 @@ system.file('data-raw', 'rawplus', package = 'clindata') %>% # path to ./data-ra
       )
     )
   })
+
