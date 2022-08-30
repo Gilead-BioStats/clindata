69e5892279c943e473879a5a03c518d17f81c5f8
Spencer Childress <spencer.c.childress@gmail.com>
2022-08-30 11:45:30 -0400

    rename ie and consent columns to match rawplus

--- data-raw/rawplus.R
+++ data-raw/rawplus.R
@@ -3,3 +3,13 @@ datasets <- rawplus_1_import()
 datasets_processed <- rawplus_2_process(datasets)
 rawplus_3_export(datasets_processed)
 rawplus_4_document(datasets_processed)
+
+# temporary fix for [ ie ]
+load('data-raw/rawplus/rawplus_ie.rda')
+names(rawplus_ie) <- c('subjid', 'iecat', 'ieorres', 'tiver')
+usethis::use_data(rawplus_ie, overwrite = TRUE)
+
+# temporary fix for [ consent ]
+load('data-raw/rawplus/rawplus_consent.rda')
+names(rawplus_consent) <- c('subjid', 'consdt', 'conscat', 'consyn')
+usethis::use_data(rawplus_consent, overwrite = TRUE)
