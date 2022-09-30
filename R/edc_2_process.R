e77161522408e0e931507945837274300c5d4cd5
Spencer Childress <spencer.c.childress@gmail.com>
2022-09-30 12:20:25 -0400

    add edc pipeline functions

---
+++ R/edc_2_process.R
@@ -0,0 +1,14 @@
+#' Process EDC data
+#'
+#' @param datasets `list` named list of data frames
+#'
+#' @importFrom magrittr %>%
+#' @importFrom purrr imap
+
+edc_2_process <- function(
+    datasets
+) {
+    datasets_processed <- datasets
+
+    datasets_processed
+}
