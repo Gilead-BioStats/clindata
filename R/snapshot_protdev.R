a00efd7b43375dbb48e3e0de941248f29911422c
Spencer Childress <spencer.c.childress@gmail.com>
2022-10-03 17:02:15 -0400

    add comment

--- R/snapshot_protdev.R
+++ R/snapshot_protdev.R
@@ -9,7 +9,7 @@ snapshot_protdev <- function(snapshot_date, dm, protdev = clindata::rawplus_prot
         visit_nsv == 'Screening' ~ -5,
         visit_nsv == 'Baseline' ~ 0,
         grepl('week', visit_nsv, TRUE) ~ readr::parse_number(visit_nsv)
-      ),
+      ), # impute date with study start date and visit when deviation date is missing
       dv_dt = if_else(
         dv_dt == '',
         impute_date(rfpst_dt) + 7*week,
