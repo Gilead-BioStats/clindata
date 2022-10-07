6acfd30733870151e2127b4360a51653aeb0166a
Matt Roumaya <matt.roumaya@atorusresearch.com>
2022-10-07 15:33:19 +0000

    move config files to clindata

--- R/config.R
+++ R/config.R
@@ -1,5 +1,17 @@
+#' @title Configuration Parameters
+#' @description Configuration Parameters (param) data CTMS data
+#' @format a data frame with 26 rows and 6 columns
+#' @source ./data-raw/config/param.csv
+"config_param"
+
 #' @title Snapshot Schedule
 #' @description Snapshot Schedule (schedule) data CTMS data
 #' @format a data frame with 75 rows and 2 columns
 #' @source ./data-raw/config/schedule.csv
 "config_schedule"
+
+#' @title Configuration Workflow
+#' @description Configuration Workflow (workflow) data CTMS data
+#' @format a data frame with 8 rows and 4 columns
+#' @source ./data-raw/config/workflow.csv
+"config_workflow"
