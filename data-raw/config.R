6bb5d2b15261efd24cdbcf101946a240ff5816ea
Matt Roumaya <matt.roumaya@atorusresearch.com>
2022-12-19 19:53:44 +0000

    update version

--- data-raw/config.R
+++ data-raw/config.R
@@ -6,8 +6,8 @@ datasets <- ctms_1_import(data_path = file.path('data-raw', 'config'))
 # Note: packageVersion() creates a class "package_version" that causes issues when joining/merging data
 # easier for us to retain as a character value
 
-datasets$param$gsm_version <- "1.4.0"
-datasets$workflow$gsm_version <- "1.4.0"
+datasets$param$gsm_version <- "1.4.1"
+datasets$workflow$gsm_version <- "1.4.1"
 
 datasets_processed <- ctms_2_process(datasets)
 ctms_3_export(datasets_processed, data_domain = "config")
