1f557d6e221c9f820248116d65368f961894963a
Spencer Childress <spencer.c.childress@gmail.com>
2022-12-19 19:25:11 -0500

    fix #102; fix #111

--- data-raw/config.R
+++ data-raw/config.R
@@ -1,17 +1,27 @@
 devtools::load_all()
 
+# import
 datasets <- ctms_1_import(data_path = file.path('data-raw', 'config'))
 
-# update description ------------------------------------------------------
-# Note: packageVersion() creates a class "package_version" that causes issues when joining/merging data
-# easier for us to retain as a character value
+gsm_releases <- gh::gh(
+    '/repos/Gilead-BioStats/gsm/releases',
+    token = remotes:::github_pat()
+)
+gsm_version <- releases[[1]]$name %>% substring(2)
 
-datasets$param$gsm_version <- "1.4.1"
-datasets$workflow$gsm_version <- "1.4.1"
+datasets$param$gsm_version <- gsm_version
+datasets$workflow$gsm_version <- gsm_version
 
+# process
 datasets_processed <- ctms_2_process(datasets)
+
+# export
 ctms_3_export(datasets_processed, data_domain = "config")
-ctms_4_document(datasets_processed,
+
+# document
+ctms_4_document(
+  datasets_processed,
   domain_metadata_path = system.file('data-standards', 'config.yaml', package = 'clindata'),
   documentation_path = paste0(system.file('R', package = 'clindata'), '/config.R'),
-  data_domain = "config")
+  data_domain = "config"
+)
