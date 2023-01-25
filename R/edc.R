a26bc4c067963521aaf808c67bd39f3151f68801
Spencer Childress <spencer.c.childress@gmail.com>
2023-01-25 14:08:16 -0500

    fix #117

--- R/edc.R
+++ R/edc.R
@@ -1,17 +1,17 @@
 #' @title Data Change Rate
 #' @description data page-level data with simulated data changes (data_change_rate)
-#' @format a data frame with 240398 rows and 7 columns
+#' @format a data frame with 247462 rows and 7 columns
 #' @source ./data-raw/edc/data_change_rate.csv
 "edc_data_change_rate"
 
 #' @title Data Entry Lag
 #' @description data page-level data with simulated data entry lag (data_entry_lag)
-#' @format a data frame with 240398 rows and 6 columns
+#' @format a data frame with 247462 rows and 6 columns
 #' @source ./data-raw/edc/data_entry_lag.csv
 "edc_data_entry_lag"
 
 #' @title Queries
 #' @description query-level data with simulated query characteristics (queries)
-#' @format a data frame with 40976 rows and 12 columns
+#' @format a data frame with 42259 rows and 12 columns
 #' @source ./data-raw/edc/queries.csv
 "edc_queries"
