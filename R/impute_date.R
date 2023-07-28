e8fecae7b97191c0797108a16f21341ca8651938
Jacob Anderson <jacob.anderson@atorusresearch.com>
2023-07-28 15:35:36 +0000

    updated impute_date to account for numerical date format

--- R/impute_date.R
+++ R/impute_date.R
@@ -2,21 +2,25 @@
 #'
 #' @export
 impute_date <- function(date) {
-  if (lubridate::is.Date(date)) {
-    return(date)
-  }
+    if (lubridate::is.Date(date)) {
+        return(date)
+    }
 
-  # no year-month-day
-  no_ymd <- which(date == "-----")
-  date[no_ymd] <- NA_character_
+    # no year-month-day
+    no_ymd <- which(date == "-----")
+    date[no_ymd] <- NA_character_
 
-  # no month-day
-  no_md <- which(grepl("^\\d{4}----$", date))
-  date[no_md] <- sub("----$", "-01-01", date[no_md])
+    # no month-day
+    no_md <- which(grepl("^\\d{4}----$", date))
+    date[no_md] <- sub("----$", "-01-01", date[no_md])
 
-  # no day
-  no_d <- which(grepl("^\\d{4}-\\d{2}--$", date))
-  date[no_d] <- sub("--$", "-01", date[no_d])
+    # no day
+    no_d <- which(grepl("^\\d{4}-\\d{2}--$", date))
+    date[no_d] <- sub("--$", "-01", date[no_d])
 
-  lubridate::ymd(date)
+    # numeric
+    num_d <- which(!grepl("\\-", date))
+    date[num_d] <- as.character(as_date(as.numeric(date[num_d])))
+
+    lubridate::ymd(date)
 }
